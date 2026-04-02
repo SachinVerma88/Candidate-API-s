842be44f0db454fb475734ee6f1cdd718369d23d
SachinVerma88 <s.verma8846@gmail.com>
2026-04-02 13:58:33 +0530

    remove unnessary things

--- candidate_api/main.py
+++ candidate_api/main.py
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-# 1️⃣ Create Candidate
+# Create Candidate
 @app.post("/candidates", response_model=schemas.CandidateResponse)
 def create_candidate(data: schemas.CandidateCreate, db: Session = Depends(get_db)):
     try:
@@ -26,13 +26,13 @@ def create_candidate(data: schemas.CandidateCreate, db: Session = Depends(get_db
         raise HTTPException(status_code=400, detail=str(e))
 
 
-# 2️⃣ Get Candidates (with optional filter)
+# Get Candidates (with optional filter)
 @app.get("/candidates", response_model=list[schemas.CandidateResponse])
 def get_candidates(status: str = None, db: Session = Depends(get_db)):
     return crud.get_candidates(db, status)
 
 
-# 3️⃣ Update Status
+# Update Status
 @app.put("/candidates/{candidate_id}/status", response_model=schemas.CandidateResponse)
 def update_status(candidate_id: int, data: schemas.UpdateStatus, db: Session = Depends(get_db)):
     candidate = crud.update_candidate_status(db, candidate_id, data.status)
